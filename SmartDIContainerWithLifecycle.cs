53df7e25927936b8caa1636fd2008d4994a3617d
Stephen Marcus <stephenmarcus123@gmail.com>
2019-10-26 12:47:09 -0700

    Package reorg and minor debbug

--- SmartDIContainerWithLifecycle.cs
+++ SmartDIContainerWithLifecycle.cs
@@ -1,51 +1,51 @@
-﻿// *********************************************************************************
-// <copyright file=SmartDIContainerWithLifecycle.cs company="Marcus Technical Services, Inc.">
-//     Copyright @2019 Marcus Technical Services, Inc.
-// </copyright>
+﻿#region License
+
+// Copyright (c) 2019  Marcus Technical Services, Inc. <marcus@marcusts.com>
 //
-// MIT License
+// This file, SmartDIContainerWithLifecycle.cs, is a part of a program called Com.MarcusTS.SmartDI.LifecycleAware.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+// Com.MarcusTS.SmartDI.LifecycleAware is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+// Permission to use, copy, modify, and/or distribute this software
+// for any purpose with or without fee is hereby granted, provided
+// that the above copyright notice and this permission notice appear
+// in all copies.
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-// *********************************************************************************
+// Com.MarcusTS.SmartDI.LifecycleAware is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// For the complete GNU General Public License,
+// see <http://www.gnu.org/licenses/>.
+
+#endregion
 
 namespace Com.MarcusTS.SmartDI.LifecycleAware
 {
-   using Com.MarcusTS.SharedForms.Common.Notifications;
+   using SharedForms.Common.Notifications;
 
    /// <summary>
-   ///    Interface ISmartDIContainerWithLifecycle
+   /// Interface ISmartDIContainerWithLifecycle
    /// </summary>
    public interface ISmartDIContainerWithLifecycle
    {
    }
 
    /// <summary>
-   ///    Class SmartDIContainerWithLifecycle.
-   ///    Implements the <see cref="Com.MarcusTS.SmartDI.SmartDIContainer" />
-   ///    Implements the <see cref="Com.MarcusTS.SmartDI.LifecycleAware.ISmartDIContainerWithLifecycle" />
+   /// Class SmartDIContainerWithLifecycle.
+   /// Implements the <see cref="Com.MarcusTS.SmartDI.SmartDIContainer" />
+   /// Implements the <see cref="Com.MarcusTS.SmartDI.LifecycleAware.ISmartDIContainerWithLifecycle" />
    /// </summary>
    /// <seealso cref="Com.MarcusTS.SmartDI.SmartDIContainer" />
    /// <seealso cref="Com.MarcusTS.SmartDI.LifecycleAware.ISmartDIContainerWithLifecycle" />
    public class SmartDIContainerWithLifecycle : SmartDIContainer, ISmartDIContainerWithLifecycle
    {
       /// <summary>
-      ///    Initializes a new instance of the <see cref="SmartDIContainerWithLifecycle" /> class.
+      /// Initializes a new instance of the <see cref="SmartDIContainerWithLifecycle" /> class.
       /// </summary>
       public SmartDIContainerWithLifecycle()
       {
@@ -53,7 +53,7 @@ namespace Com.MarcusTS.SmartDI.LifecycleAware
       }
 
       /// <summary>
-      ///    Called when [object disappearing].
+      /// Called when [object disappearing].
       /// </summary>
       /// <param name="sender">The sender.</param>
       /// <param name="mess">The mess.</param>
