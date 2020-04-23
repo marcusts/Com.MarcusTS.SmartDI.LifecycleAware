08fb5290ee061260773209ef9b1b96fcb12a107c
Stephen Marcus <marcus@marcusts.com>
2020-04-22 18:26:49 -0700

    Updates for Current SharedForms and ShartedUtils

--- SmartDIContainerWithLifecycle.cs
+++ SmartDIContainerWithLifecycle.cs
@@ -1,51 +1,25 @@
-﻿#region License
-
-// Copyright (c) 2019  Marcus Technical Services, Inc. <marcus@marcusts.com>
-//
-// This file, SmartDIContainerWithLifecycle.cs, is a part of a program called Com.MarcusTS.SmartDI.LifecycleAware.
-//
-// Com.MarcusTS.SmartDI.LifecycleAware is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Permission to use, copy, modify, and/or distribute this software
-// for any purpose with or without fee is hereby granted, provided
-// that the above copyright notice and this permission notice appear
-// in all copies.
-//
-// Com.MarcusTS.SmartDI.LifecycleAware is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// For the complete GNU General Public License,
-// see <http://www.gnu.org/licenses/>.
-
-#endregion
-
-namespace Com.MarcusTS.SmartDI.LifecycleAware
+﻿namespace Com.MarcusTS.SmartDI.LifecycleAware
 {
-   using SharedForms.Common.Notifications;
+   using Com.MarcusTS.SharedForms.Common.Notifications;
 
    /// <summary>
-   /// Interface ISmartDIContainerWithLifecycle
+   ///    Interface ISmartDIContainerWithLifecycle
    /// </summary>
    public interface ISmartDIContainerWithLifecycle
    {
    }
 
    /// <summary>
-   /// Class SmartDIContainerWithLifecycle.
-   /// Implements the <see cref="Com.MarcusTS.SmartDI.SmartDIContainer" />
-   /// Implements the <see cref="Com.MarcusTS.SmartDI.LifecycleAware.ISmartDIContainerWithLifecycle" />
+   ///    Class SmartDIContainerWithLifecycle.
+   ///    Implements the <see cref="Com.MarcusTS.SmartDI.SmartDIContainer" />
+   ///    Implements the <see cref="Com.MarcusTS.SmartDI.LifecycleAware.ISmartDIContainerWithLifecycle" />
    /// </summary>
    /// <seealso cref="Com.MarcusTS.SmartDI.SmartDIContainer" />
    /// <seealso cref="Com.MarcusTS.SmartDI.LifecycleAware.ISmartDIContainerWithLifecycle" />
    public class SmartDIContainerWithLifecycle : SmartDIContainer, ISmartDIContainerWithLifecycle
    {
       /// <summary>
-      /// Initializes a new instance of the <see cref="SmartDIContainerWithLifecycle" /> class.
+      ///    Initializes a new instance of the <see cref="SmartDIContainerWithLifecycle" /> class.
       /// </summary>
       public SmartDIContainerWithLifecycle()
       {
@@ -53,7 +27,7 @@ namespace Com.MarcusTS.SmartDI.LifecycleAware
       }
 
       /// <summary>
-      /// Called when [object disappearing].
+      ///    Called when [object disappearing].
       /// </summary>
       /// <param name="sender">The sender.</param>
       /// <param name="mess">The mess.</param>
--- SmartDIContainerWithLifecycleForUnitTesting.cs
+++ SmartDIContainerWithLifecycleForUnitTesting.cs
@@ -1,36 +1,10 @@
-﻿#region License
-
-// Copyright (c) 2019  Marcus Technical Services, Inc. <marcus@marcusts.com>
-//
-// This file, SmartDIContainerWithLifecycleForUnitTesting.cs, is a part of a program called Com.MarcusTS.SmartDI.LifecycleAware.
-//
-// Com.MarcusTS.SmartDI.LifecycleAware is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Permission to use, copy, modify, and/or distribute this software
-// for any purpose with or without fee is hereby granted, provided
-// that the above copyright notice and this permission notice appear
-// in all copies.
-//
-// Com.MarcusTS.SmartDI.LifecycleAware is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// For the complete GNU General Public License,
-// see <http://www.gnu.org/licenses/>.
-
-#endregion
-
-namespace Com.MarcusTS.SmartDI.LifecycleAware
+﻿namespace Com.MarcusTS.SmartDI.LifecycleAware
 {
-   using SharedForms.Common.Notifications;
+   using Com.MarcusTS.SharedForms.Common.Notifications;
 
    /// <summary>
-   /// Interface ISmartDIContainerWithLifecycleForUnitTesting
-   /// Implements the <see cref="Com.MarcusTS.SmartDI.ISmartDIContainerForUnitTesting" />
+   ///    Interface ISmartDIContainerWithLifecycleForUnitTesting
+   ///    Implements the <see cref="Com.MarcusTS.SmartDI.ISmartDIContainerForUnitTesting" />
    /// </summary>
    /// <seealso cref="Com.MarcusTS.SmartDI.ISmartDIContainerForUnitTesting" />
    public interface ISmartDIContainerWithLifecycleForUnitTesting : ISmartDIContainerForUnitTesting
@@ -38,9 +12,9 @@ namespace Com.MarcusTS.SmartDI.LifecycleAware
    }
 
    /// <summary>
-   /// Class SmartDIContainerWithLifecycleForUnitTesting.
-   /// Implements the <see cref="Com.MarcusTS.SmartDI.SmartDIContainerForUnitTesting" />
-   /// Implements the <see cref="Com.MarcusTS.SmartDI.LifecycleAware.ISmartDIContainerWithLifecycleForUnitTesting" />
+   ///    Class SmartDIContainerWithLifecycleForUnitTesting.
+   ///    Implements the <see cref="Com.MarcusTS.SmartDI.SmartDIContainerForUnitTesting" />
+   ///    Implements the <see cref="Com.MarcusTS.SmartDI.LifecycleAware.ISmartDIContainerWithLifecycleForUnitTesting" />
    /// </summary>
    /// <seealso cref="Com.MarcusTS.SmartDI.SmartDIContainerForUnitTesting" />
    /// <seealso cref="Com.MarcusTS.SmartDI.LifecycleAware.ISmartDIContainerWithLifecycleForUnitTesting" />
@@ -48,7 +22,7 @@ namespace Com.MarcusTS.SmartDI.LifecycleAware
                                                               ISmartDIContainerWithLifecycleForUnitTesting
    {
       /// <summary>
-      /// Initializes a new instance of the <see cref="SmartDIContainerWithLifecycleForUnitTesting" /> class.
+      ///    Initializes a new instance of the <see cref="SmartDIContainerWithLifecycleForUnitTesting" /> class.
       /// </summary>
       public SmartDIContainerWithLifecycleForUnitTesting()
       {
@@ -56,7 +30,7 @@ namespace Com.MarcusTS.SmartDI.LifecycleAware
       }
 
       /// <summary>
-      /// Called when [object disappearing].
+      ///    Called when [object disappearing].
       /// </summary>
       /// <param name="sender">The sender.</param>
       /// <param name="mess">The mess.</param>
